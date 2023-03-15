be652aa9c51076a21947a38104eeb31a30135710
Wannes Lambeens <Wannes.lambeens@student.ehb.be>
2023-03-15 15:45:50 +0100

    recieving messages

--- js/index.js
+++ js/index.js
@@ -3,11 +3,42 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
     },
     fetchMessages() {
-    },
+        fetch('https://dev2chat.onrender.com/messages')
+        .then(function (response) {
+        return response.json();
+        }) 
+        .then(function(messages) {
+        console.log(messages);
+
+
+
+      messages.forEach(messageAmount => {
+
+        const date = new Date(messageAmount.created_at);
+
+        document.querySelector('#messageContainer').insertAdjacentHTML("beforeend", `        
+        <div class="messageItem">
+            <div class="header">
+                <span class="author">${messageAmount.author}</span>
+                <span class="time">${date}</span>
+            </div>
+            <p class="message">
+            ${messageAmount.message}
+            </p>
+        </div>
+        `
+        );
+      });
+        
+    }
+    
+
+   )},
     renderMessage(message) {
     }
 
